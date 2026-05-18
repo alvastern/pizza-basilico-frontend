a9a605dda118caf02c90f329782a7ee6fcb0d133
alvastern <alva.stern02@gmail.com>
2026-05-18 20:54:47 +0200

    Skapande av menyns innehåll

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
-import { Footer } from './components/footer/footer'
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+
+export class Home implements OnInit {
+
+  pizzas: any[] = [];
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+
+    this.http.get<any[]>("http://localhost:3000/menu")
+      .subscribe(data => {
+
+        this.pizzas = data;
+        this.cdr.detectChanges();
+
+      });
+  }
+}
