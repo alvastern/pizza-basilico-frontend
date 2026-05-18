28e308201bd7a1fbe5eafe7b7f8cd45c0d07f7c1
alvastern <alva.stern02@gmail.com>
2026-05-18 14:42:26 +0200

    Skapande av routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+import { BokaBordInfo } from './pages/boka-bord/boka-bord-info/boka-bord-info';
+import { BokaBordKontakt } from './pages/boka-bord/boka-bord-kontakt/boka-bord-kontakt';
+import { BokaBordTack } from './pages/boka-bord/boka-bord-tack/boka-bord-tack';
+
+import { Order } from './pages/takeaway/takeaway-order/order';
+import { TakeawayPay } from './pages/takeaway/takeaway-pay/takeaway-pay';
+import { TakeawayTack } from './pages/takeaway/takeaway-tack/takeaway-tack';
+
+export const routes: Routes = [
+
+    { path: '', component: Home },
+
+    { path: 'boka-bord', component: BokaBordInfo },
+    { path: 'boka-bord/kontakt', component: BokaBordKontakt },
+    { path: 'boka-bord/tack', component: BokaBordTack },
+
+    { path: 'takeaway', component: Order },
+    { path: 'takeaway/pay', component: TakeawayPay },
+    { path: 'takeaway/tack', component: TakeawayTack }
+
+];
