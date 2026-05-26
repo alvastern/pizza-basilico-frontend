98e31e27375bb65b475f421612b1ea351a687839
alvastern <alva.stern02@gmail.com>
2026-05-26 08:51:54 +0200

    Skapat en grund för ADMIN-delen, register och login + validering i delen för att skapa ett konto

--- src/app/pages/admin/login/login.ts
+++ src/app/pages/admin/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderAdmin } from "../../../components/header/header-admin/header-admin";
+import { RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [HeaderAdmin, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
--- src/app/pages/admin/register/register.ts
+++ src/app/pages/admin/register/register.ts
@@ -1,9 +1,67 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HeaderAdmin } from "../../../components/header/header-admin/header-admin";
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, HeaderAdmin],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+
+export class Register {
+
+  username = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  errorMessage = '';
+
+  constructor(private http: HttpClient) {}
+
+  registerUser() {
+    if(
+      !this.username ||
+      !this.email ||
+      !this.password ||
+      !this.confirmPassword
+    ) {
+
+      this.errorMessage =
+        'Alla fält måste fyllas i';
+        return;
+    }
+
+    if(this.password !== this.confirmPassword) {
+      this.errorMessage =
+        'Lösenorden matchar inte';
+
+      return;
+    }
+
+    if(this.password.length < 6) {
+      this.errorMessage =
+        'Lösenordet måste vara minst 6 tecken';
+
+      return;
+    }
+
+    this.errorMessage = '';
+
+    const userData = {
+      username: this.username,
+      email: this.email,
+      password: this.password
+
+    };
+
+    this.http.post(
+      'http://localhost:3000/register',
+      userData
+    ).subscribe(response => {
+
+      console.log(response);
+    });
+  }
+}
