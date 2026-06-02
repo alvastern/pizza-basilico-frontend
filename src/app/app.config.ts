da6683dff826d7d4a2febbb1a8795a57e95096e2
alvastern <alva.stern02@gmail.com>
2026-06-02 10:17:09 +0200

    Kommentarer för samtliga filer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
+// Innehåller providers för applikationen, routing och HTTP-klient
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
--- src/app/pages/takeaway/takeaway-tack/takeaway-tack.ts
+++ src/app/pages/takeaway/takeaway-tack/takeaway-tack.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Header } from "../../../components/header/header";
 
+// Visar tack-sida för när en beställning har gjorts
 @Component({
   selector: 'app-takeaway-tack',
   imports: [CommonModule, RouterLink, Header],
